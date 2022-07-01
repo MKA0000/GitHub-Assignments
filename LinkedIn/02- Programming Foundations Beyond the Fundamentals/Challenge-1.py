82c6fc30a91ee87fa4dd50045c6954a8b80a1378
Mohammed Khaled <ma0068664@gmail.com>
2022-07-01 10:51:07 +0200

    linkedin and HTML&CSS course assignmentes is completed

---
+++ LinkedIn/02- Programming Foundations Beyond the Fundamentals/Challenge-1.py
@@ -0,0 +1,23 @@
+# Nearest stars to Earth
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359'
+]
+print(stars[3])
+
+
+# Highest peak on each tectonic plate
+
+
+peaks = {
+    'Antarctic': 'Vinson',
+    'African': 'Kilimanjaro',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua'
+}
+print(peaks['Pacific'])
---
+++ LinkedIn/02- Programming Foundations Beyond the Fundamentals/Challenge-3.py
@@ -0,0 +1,6 @@
+# Convert from miles to killometer
+
+miles = input('Enter the distance: ')
+killometer = float(miles) * 1.60934
+print('The distance in km = ')
+print(killometer)
